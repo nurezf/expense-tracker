ab07a13a65a08120e339684114eb12cafe72ab81
nurezf <nuredinzf1996@gmail.com>
2024-01-11 13:41:35 -0800

    final

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function addExpense(){
     let amount = document.getElementById('amount').value;
     let date = document.getElementById('date').value;
     let quantity = document.getElementById('number').value;
-    let row = document.getElementById("row");
-    row.innerHTML = `<td>${expense}</td><td>${amount}</td><td>${date}</td><td>${quantity}</td>`;
+    let row = document.getElementById("table");
+    let a = document.createElement("tr");
+    a.innerHTML= `<td>${expense}</td><td>${amount}</td><td>${date}</td><td>${quantity}</td>`;
+    row.appendChild(a);
 }
